f90e4991e5d1f31061a844fba81323a5b8549470
servosdesign <servos.design0@gmail.com>
2022-10-24 18:23:48 -0500

    javascript email, password, username, form validation

--- js/validation.js
+++ js/validation.js
@@ -1,31 +1,66 @@
 function validate() {
+  const userName = document.registrationForm.UserName.value;
+  const userNameFocus = document.registrationForm.UserName.focus();
+  const email = document.registrationForm.EMail.value;
+  const password = document.registrationForm.Password.value;
+  const passwordFocus = document.registrationForm.Password.focus();
+  const confirmPassword = document.registrationForm.ConfirmPassword.value;
+  const confirmPasswordFocus = document.registrationForm.ConfirmPassword.focus();
 
-const userName = document.registrationForm.UserName.value;
-const email = document.registrationForm.EMail.value;
-const password = document.registrationForm.Password.value
-const confirmPassword = document.registrationForm.ConfirmPassword.value;
+  var userNameLetters = /^[A-Za-z]+$/;
+  var userNameLength = /^.{3,}$/;
+  
+  var passwordLetters = /[A-Z].*\d|\d.*[A-Z]/; 
+  var passwordDigits = /^[/*-+!@#$^&~[]]/; 
+  var passwordLength = /^.{8,}$/;
 
   if (userName == "") {
-    alert("Please provide your userName!");
-    document.registrationForm.UserName.focus();
+    alert("Username required");
+    userNameFocus;
+    return false;
+  }
+  if (!userName.match(userNameLetters)) {
+    alert("Make sure username starts with a letter A-Z");
+    userNameFocus;
+    return false;
+  }
+  if (!userName.match(userNameLength)) {
+    alert("Make sure username is 3 or more alphanumeric characters long");
+    userNameFocus;
     return false;
   }
 
   if (email == "") {
-    alert("Please provide your Email!");
+    alert("Email Required");
     document.registrationForm.EMail.focus();
     return false;
   }
 
   if (password == "") {
-    alert("Please provide your password!");
-    document.registrationForm.Password.focus();
+    alert("Password Required");
+    passwordFocus;
+    return false;
+  }
+  if (!password.match(passwordLength)){
+    alert("Make sure password is 8 or more alphanumeric characters long");
+    passwordFocus;
+    return false;
+  }
+  if (!password.match(passwordLetters) && !password.match(passwordDigits)) {
+    alert("Make sure password is contains at least one upper case letter, one number, and one of the following special characters: / * - + ! @ # $ ^ & ~ [ ]");
+    passwordFocus;
     return false;
   }
 
-  if (confirmPassword.value == "") {
-    alert("Please provide your password!");
-    document.registrationForm.ConfirmPassword.focus();
+  if (confirmPassword == "") {
+    alert("Confirm password");
+    confirmPasswordFocus;
+    return false;
+  }
+
+  if (password != confirmPassword){
+    alert("Make sure both passwords match exactly")
+    confirmPasswordFocus;
     return false;
   }
 }
